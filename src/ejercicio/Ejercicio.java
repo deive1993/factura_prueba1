a76a461a8287cde73742745c017e71dd4e99bc3d
deive1993 <you@example.com>
2017-06-29 23:44:02 -0300

    modificacion factura java

---
+++ src/ejercicio/Ejercicio.java
@@ -0,0 +1,17 @@
+
+package ejercicio;
+
+import javax.swing.JOptionPane;
+
+
+public class Ejercicio {
+
+    public static void main(String[] args) {
+        int a;
+        a = 24;
+        
+        
+        JOptionPane.showMessageDialog(null, "Mi edad es de : " +a+ " y mi nombre es deivis.");
+    }
+    
+}
